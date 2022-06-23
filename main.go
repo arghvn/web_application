5b7c3c342cfece5b3daeae472bf836ee5014667e
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-23 10:35:53 +0430

    initial

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 // lemonade application for buy juices
+// creating a resource server for our application
 
 import (
-	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("start shop")
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		// w is an object that implements http.ResponseWriter , instead using object in req we use pointer to http.Request
+		w.Write([]byte("Hello , im writting a shop"))
+		// its take two argument ,pattern string and handler func(ResponseWriter, *Request), / use for route in project
+	})
+	http.ListenAndServe(":8000", nil)
 }
