c50c599881b6876e974366ea695eeeb4f908b8d3
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-23 10:49:59 +0430

    error handling

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 // creating a resource server for our application
 
 import (
+	"io/ioutil"
 	"net/http"
 )
 
@@ -14,4 +15,22 @@ func main() {
 		// its take two argument ,pattern string and handler func(ResponseWriter, *Request), / use for route in project
 	})
 	http.ListenAndServe(":8000", nil)
+}
+
+type MyHandler struct {
+	http.Handler
+}
+
+// HTTP handler functions in Go must have a very particular signature:
+// func(http.ResponseWriter, *http.Request).
+//  The web server implementation in the standard library needs to call these functions, so they must conform to a type declared in the http package, and that type only allows for those two parameters.
+func (this *MyHandler) SrveHttp(w http.ResponseWriter, req *http.Request) {
+	path := "public/" + req.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+	if err == nil {
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 -" + http.StatusText(404)))
+	}
 }
