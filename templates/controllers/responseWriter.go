c293f4e8b26b195d05d6939049eecc2494c742e2
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-07-04 22:08:24 +0430

    new part without comment

---
+++ templates/controllers/responseWriter.go
@@ -0,0 +1,50 @@
+package util
+
+import (
+	"compress/gzip"
+	"net/http"
+)
+
+type ClosableResponseWriter interface {
+	// interfaces in future
+	http.ResponseWriter
+	close()
+}
+
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	*gzip.Writer
+}
+
+func (this gzipResponseWriter) Writer(data []byte) (int, error) {
+	return this.Writer.Write(data)
+}
+
+func (this gzipResponseWriter) close() {
+	this.Writer.close()
+}
+
+func (this gzipResponseWriter) Header() http.Header {
+	return this.ResponseWriter.Header()
+}
+
+type ClosableResponseWriter struct {
+	http.ResponseWriter
+}
+
+func (this ClosableResponseWriter) close() {
+
+}
+
+func gzipResponseWriter(w http.ResponseWriter, req *http.Request) {
+	if strings.contains(req.Header.Get("accept-encoding"), "gzip") {
+		w.Header().set("content-encoding", "gzip")
+		gRW := gzipResponseWriter{
+			ResponseWriter: w,
+			writer:         gzip.NewWriter(w),
+		}
+		return gRW
+	} else {
+		return closeableResponeWriter{Responsewriter: w}
+	}
+}
