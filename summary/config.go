81e03736d8885777f64a683b677c18fd67996039
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-07-08 22:17:25 +0430

    database sql

--- summary/config.go
+++ summary/config.go
@@ -2,8 +2,16 @@ package config
 
 // leraning details in future
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 func GetDB() (db *sql.DB, err error) {
+	dbDriver := "mysql"
+	dbName := "db6"
+	dbUser := "root"
+	dbPass := "123456"
+	db, err = sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	return
 
 }
